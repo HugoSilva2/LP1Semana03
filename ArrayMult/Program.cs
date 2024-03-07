a6c8a22ed0ba76dfbebaf9060ed038929ae90476
HugoSilva2 <hugomiguelsarmentodasilva@gmail.com>
2024-03-07 23:41:56 +0000

    Perks Done

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for()
+            {
+
+            }
+        }
+    }
+}
